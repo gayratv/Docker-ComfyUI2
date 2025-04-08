0c1c496c5fc74f0056c41fbecae142df3a3e3c89
Gayrat Vlasov <ggvlasov@gmail.com>
2025-04-08 22:03:54 +0300

    vast offers python ver1

--- VAST.AI/vast-search-py.py
+++ VAST.AI/vast-search-py.py
@@ -1,16 +1,65 @@
+import requests
 import json
+from urllib.parse import urlencode
 
 # Указываем путь к вашему JSON файлу
-file_path = 'vast-search.json'
+file_path_json = 'vast-search.json'
+
+
+# URL для запроса
+url = "https://cloud.vast.ai/api/v0/search/asks/"
+
+# Параметры запроса
+query_data = {
+    "q": json.dumps({
+        "gpu_name": {"eq": "RTX 3090"},
+        "num_gpus": {"eq": 1},
+        "rentable": {"eq": True},
+        "cuda_max_good": {"gte": "12.4"},
+        "cpu_ram": {"gte": "32000"},
+        "inet_down": {"gte": "1000.0"},
+        "gpu_frac": {"gte": "0.5"},
+        "disk_space": {"gte": "100.0"}
+    })
+}
+
+# Кодирование параметров с помощью urlencode
+encoded_params = urlencode(query_data)
+
+# Полный URL с закодированными параметрами
+full_url = f"{url}?{encoded_params}"
+
+# Выполнение GET-запроса
+response = requests.get(full_url)
+
+# Проверка статуса ответа
+if response.status_code == 200:
+    data = response.json()
+
+    # Проверка наличия ключа 'offers' в ответе
+    if "offers" in data and isinstance(data["offers"], list):
+        # Подсчет количества записей в массиве offers
+        offer_count = len(data["offers"])
+        print(f"Количество записей в массиве 'offers': {offer_count}")
+
+    # Сохранение результата в файл vast-search.json
+    with open(file_path_json, "w") as file:
+        json.dump(data, file, indent=4)
+    print("Данные успешно сохранены в файл vast-search.json\n")
+else:
+    print(f"Ошибка при выполнении запроса: {response.status_code}")
+    print(f"Ответ сервера: {response.text}")
+
+# exit(0)
 
 # Открываем и читаем файл
-with open(file_path, 'r', encoding='utf-8') as file:
+with open(file_path_json, 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 # Проверяем, что в данных есть список offers
 if 'offers' in data:
     # Выводим заголовок таблицы
-    print(f"{'ID':<10} {'CPU RAM':<10} {'GPU Name':<15} {'Inet Down':<12} {'IDown Cost':<15} {'IDown Cost TB':<15}  {'Setup Cost':<12} {'Machine ID':<12} {'Total Hour':<12}")
+    print(f"{'ID':<10} {'CPU RAM':<10} {'GPU Name':<15} {'Inet Down':<12} {'IDown Cost':<15} {'IDown Cost TB':<15}  {'Setup Cost':<12} {'Machine ID':<12} {'$/HR':<12}")
     print("-" * 97)  # Разделительная линия
 
     for offer in data['offers']:
