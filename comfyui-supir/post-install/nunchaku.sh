dfd5fbef44caf691927ede5b04f7f73429f3434c
Gayrat Vlasov <ggvlasov@gmail.com>
2025-04-30 21:19:53 +0300

    nunchaku убрал ноду переводчик

--- comfyui-supir/post-install/nunchaku.sh
+++ comfyui-supir/post-install/nunchaku.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-#pip install --cache-dir /root/pip-cache nunchaku
-#pip install https://huggingface.co/mit-han-lab/nunchaku/resolve/main/nunchaku-0.2.0+torch2.6-cp311-cp311-linux_x86_64.whl
-#pip install --cache-dir /root/pip-cache  https://github.com/mit-han-lab/nunchaku/releases/download/v0.2.0/nunchaku-0.2.0+torch2.6-cp311-cp311-linux_x86_64.whl
-#pip install --cache-dir /root/pip-cache  "https://huggingface.co/mit-han-lab/nunchaku/resolve/main/nunchaku-0.1.3+torch2.6-cp311-cp311-linux_x86_64.whl"
 
-#https://github.com/mit-han-lab/nunchaku/releases/tag/v0.2.0
 pip install --cache-dir /root/pip-cache  "https://github.com/mit-han-lab/nunchaku/releases/download/v0.2.0/nunchaku-0.2.0+torch2.6-cp311-cp311-linux_x86_64.whl"
-#pip install "https://github.com/mit-han-lab/nunchaku/releases/download/v0.2.0/nunchaku-0.2.0+torch2.6-cp311-cp311-linux_x86_64.whl"
+
+
+# Для Deepl транслятор
+#mkdir /temp
+#cd /temp
+#wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+#tar -xzf go1.24.2.linux-amd64.tar.gz -C /workspace/ComfyUI/custom_nodes/ComfyUI_Custom_Nodes_AlekPet/DeepLXTranslateNode
+#cd /workspace/ComfyUI/custom_nodes/ComfyUI_Custom_Nodes_AlekPet/DeepLXTranslateNode
+#git clone https://github.com/OwO-Network/DeepLX
+
+#echo 'export IP=127.0.0.1' >> /root/.bashrc
