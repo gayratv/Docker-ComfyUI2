c44132ab7691ee4e6264792bfc11f362637217ff
Gayrat Vlasov <ggvlasov@gmail.com>
2025-05-17 09:16:17 +0300

    InfiniteYou start

--- VAST.AI/strt.sh
+++ VAST.AI/strt.sh
@@ -18,4 +18,5 @@ fi
 cd /mnt/f/_prg/python/Docker-ComfyUI/VAST.AI
 source /mnt/f/_prg/python/Docker-ComfyUI/.venv/bin/activate
 export IMAGE_NAME="$1"
-python3 vast-search-py3.py --ram 32000 --gpu1 "RTX 4090" --gpu2 "RTX 3090" --cuda_max_good "$2"
+#python3 vast-search-py3.py --ram 32000 --gpu1 "RTX 4090" --gpu2 "RTX 3090" --cuda_max_good "$2"
+python3 vast-search-py3.py --ram 32000 --gpu1 "RTX 4090" --cuda_max_good "$2"
---
+++ comfyui-union2/workflow-descr/wan-2.1/readme.md
@@ -0,0 +1,2 @@
+https://wan.video/
+https://boosty.to/stabledif/posts/1a047862-3c2a-495f-92f1-1a583937ef11
