a2338154c237ebebd8c95abfbed4e2881bd84b8a
Gayrat Vlasov <ggvlasov@gmail.com>
2025-05-19 14:24:06 +0300

    character-v3

---
+++ comfyui-union2/workflow-descr/character-v3/Flux vae [319.77MB].bat
@@ -0,0 +1,20 @@
+set "DOWNLOAD_URL=https://huggingface.co/StableDiffusionVN/Flux/resolve/main/Vae/flux_vae.safetensors"
+set "MODEL_NAME=ae.safetensors"
+set "TARGET_DIR=App\ComfyUI\models\vae\FLUX1"
+
+
+set "DOWNLOAD_URL=https://huggingface.co/InstantX/InstantID/resolve/main/ControlNetModel/diffusion_pytorch_model.safetensors?download=true"
+set "MODEL_NAME=diffusion_pytorch_model.safetensors"
+set "TARGET_DIR=App\ComfyUI\models\controlnet"
+
+set "DOWNLOAD_URL=https://huggingface.co/InstantX/InstantID/resolve/main/ip-adapter.bin?download=true"
+set "MODEL_NAME=ip-adapter.bin"
+set "TARGET_DIR=App\ComfyUI\models\instantid"
+
+set "DOWNLOAD_URL=https://huggingface.co/alexgenovese/reica_models/resolve/021e192bd744c48a85f8ae1832662e77beb9aac7/realvisxlV40_v40LightningBakedvae.safetensors"
+set "MODEL_NAME=realvisxlV40_v40LightningBakedvae.safetensors"
+set "TARGET_DIR=App\ComfyUI\models\checkpoints"
+
+set "DOWNLOAD_URL=https://huggingface.co/datasets/Gourieff/ReActor/resolve/main/models/sams/sam_vit_b_01ec64.pth"
+set "MODEL_NAME=sam_vit_b_01ec64.pth"
+set "TARGET_DIR=App\ComfyUI\models\sams"
