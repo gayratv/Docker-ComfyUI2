e7acb924920cbfb5a13528398794b4a561eda984
Gayrat Vlasov <ggvlasov@gmail.com>
2025-05-20 12:58:05 +0300

    Fluxgym поправил скрипты

--- Fluxgym/bash-scripts/comfy-venv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /workspace/.venv/bin/activate
-cd /workspace/ComfyUI
-python main.py  --listen 0.0.0.0 --port 8188
--- Fluxgym/dr.sh
+++ Fluxgym/dr.sh
@@ -7,9 +7,7 @@ export $(grep -v '^#' .env | xargs)
 
 echo "run --name $IMAGE_NAME $IMAGE_NAME:$VERSION"
 docker rm -f "$IMAGE_NAME"
-docker run -it --privileged --gpus all -p 8188:8188 -p 1188:1188 \
-    -e "GOOGLE_API=$GOOGLE_API" \
-    -e "google_api_key=$GOOGLE_API" \
+docker run -it --privileged --gpus all -p 7860:7860  \
     -v /mnt/h/ComfyUI-data/models:/workspace/ComfyUI/models \
     --name "$IMAGE_NAME" "$IMAGE_NAME:$VERSION" \
     bash
