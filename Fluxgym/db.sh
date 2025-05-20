dbef9dc8645efa8daae9c108fb9e85b5c46aae98
Gayrat Vlasov <ggvlasov@gmail.com>
2025-05-20 11:53:09 +0300

    Fluxgym удалил input

--- Fluxgym/db.sh
+++ Fluxgym/db.sh
@@ -5,16 +5,11 @@
 #    --no-cache \
 docker build --progress=plain \
     --build-arg BASE_IMAGE="$BASE_IMAGE" \
-    --build-arg NODES="$NODES" \
-    --build-arg POST_INSTALL=$POST_INSTALL \
-    --build-arg WORKFLOW_TO_COPY=$WORKFLOW_TO_COPY \
     --build-arg MODELS=$MODELS \
-    --build-arg COMFYUI_VERSION=$COMFYUI_VERSION \
     --build-arg PYTORCH_WHEEL=$PYTORCH_WHEEL \
-    --build-arg REQ_MODIFY=$REQ_MODIFY \
-    -f /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2/Dockerfile \
+    -f /mnt/f/_prg/python/Docker-ComfyUI/Fluxgym/Dockerfile \
     -t $IMAGE_NAME:$VERSION \
-    /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2
+    /mnt/f/_prg/python/Docker-ComfyUI/Fluxgym
 
 echo -e "\nсобран образ $IMAGE_NAME:$VERSION"
 
