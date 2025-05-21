240136b23f8e24e8542545ecabee4f0b35565884
Gayrat Vlasov <ggvlasov@gmail.com>
2025-05-21 12:33:57 +0300

    docker из официального repo

---
+++ fluxgym/db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#    --no-cache \
+#    --cache-from $IMAGE_NAME:$VERSION \
+#    --no-cache \
+docker build --progress=plain \
+    --build-arg BASE_IMAGE="$BASE_IMAGE" \
+    --build-arg MODELS=$MODELS \
+    --build-arg PYTORCH_WHEEL=$PYTORCH_WHEEL \
+    -f /mnt/f/_prg/python/Docker-ComfyUI/Fluxgym/Dockerfile.cuda12.4 \
+    -t fluxgym:of-repo \
+    /mnt/f/_prg/python/Docker-ComfyUI/fluxgym
+
+echo -e "\nсобран образ $IMAGE_NAME:$VERSION"
+
+# docker builder prune --all
