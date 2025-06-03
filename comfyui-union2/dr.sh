5803626e99dfa1444d93f5bea89e3c37ccd3e588
Gayrat Vlasov <ggvlasov@gmail.com>
2025-06-03 08:41:07 +0300

    Gayrat node test

--- comfyui-union2/dr.sh
+++ comfyui-union2/dr.sh
@@ -8,8 +8,7 @@
 echo "run --name $IMAGE_NAME $IMAGE_NAME:$VERSION"
 docker rm -f "$IMAGE_NAME"
 docker run -it --privileged --gpus all -p 8188:8188 -p 1188:1188 \
-    -e "GOOGLE_API=$GOOGLE_API" \
-    -e "google_api_key=$GOOGLE_API" \
+    --env-file .env \
     -v /mnt/h/ComfyUI-data/models:/workspace/ComfyUI/models \
     --name "$IMAGE_NAME" "$IMAGE_NAME:$VERSION" \
     tmux-s-bash.sh
