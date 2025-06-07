ff45912939d5dbd14f438b9f77d1e583663755f9
Gayrat Vlasov <ggvlasov@gmail.com>
2025-06-07 04:31:31 +0300

    Nunchaku

--- comfyui-union2/post-install/nunchaku.sh
+++ comfyui-union2/post-install/nunchaku.sh
@@ -6,5 +6,11 @@ set -x  # Логирование всех команд
 #pip install https://huggingface.co/mit-han-lab/nunchaku/resolve/main/nunchaku-0.2.0%2Btorch2.7-cp311-cp311-linux_x86_64.whl
 pip install --cache-dir=/root/pip-cache  https://huggingface.co/mit-han-lab/nunchaku/resolve/main/nunchaku-0.3.0%2Btorch2.7-cp311-cp311-linux_x86_64.whl
 
+# nunchaku-0.3.0+torch2.7-cp311-cp311-linux_x86_64.whl
+# https://huggingface.co/mit-han-lab/nunchaku/resolve/main/nunchaku-0.3.0%2Btorch2.7-cp311-cp311-linux_x86_64.whl
+
+
+pip install --cache-dir=/root/pip-cache  apex
+
 rm -rf /root/.cache/pip
 
