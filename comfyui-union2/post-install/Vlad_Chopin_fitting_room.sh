9d7136de15727b4dc8c3591a8c1680abf81cdc2b
Gayrat Vlasov <ggvlasov@gmail.com>
2025-06-19 22:22:18 +0300

    aqua_paint_civitai

--- comfyui-union2/post-install/Vlad_Chopin_fitting_room.sh
+++ comfyui-union2/post-install/Vlad_Chopin_fitting_room.sh
@@ -3,9 +3,13 @@
 set -e  # Выход при ошибке
 set -x  # Логирование всех команд
 
-pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless
+#pip uninstall -y opencv-python opencv-contrib-python opencv-python-headless
+## uninstalled opencv-python-4.11.0.86
+#
+#pip install --cache-dir=/root/pip-cache opencv-python==4.7.0.72
 
-pip install --cache-dir=/root/pip-cache opencv-python==4.7.0.72
+
+
 #pip install --cache-dir=/root/pip-cache -U "numpy==2.2.3"
 
 rm -rf /root/.cache/pip
