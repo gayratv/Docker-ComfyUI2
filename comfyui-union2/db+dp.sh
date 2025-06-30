6eebb6dcf860423c10766d2a94556432645e0202
Gayrat Vlasov <ggvlasov@gmail.com>
2025-06-30 12:28:57 +0300

    FluxKontext

--- comfyui-union2/db+dp.sh
+++ comfyui-union2/db+dp.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
 
-
-docker build --progress=plain \
-    --build-arg BASE_IMAGE="$BASE_IMAGE" \
-    --build-arg NODES="$NODES" \
-    --build-arg POST_INSTALL=$POST_INSTALL \
-    --build-arg WORKFLOW_TO_COPY=$WORKFLOW_TO_COPY \
-    --build-arg MODELS=$MODELS \
-    --build-arg COMFYUI_VERSION=$COMFYUI_VERSION \
-    --build-arg PYTORCH_WHEEL=$PYTORCH_WHEEL \
-    --build-arg REQ_MODIFY=$REQ_MODIFY \
-    -f /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2/Dockerfile \
-    -t $IMAGE_NAME:$VERSION \
-    /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2
-
-echo -e "\nсобран образ $IMAGE_NAME:$VERSION"
-
-
-echo "docker push gayrat/$IMAGE_NAME:$VERSION"
-
-docker tag "$IMAGE_NAME:$VERSION" gayrat/"$IMAGE_NAME:$VERSION"
-
-docker push gayrat/"$IMAGE_NAME:$VERSION"
+source db.sh
+source dp.sh
--- comfyui-union2/db+dr.sh
+++ comfyui-union2/db+dr.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
 
-#    --no-cache \
-#    --cache-from $IMAGE_NAME:$VERSION \
-#    --no-cache \
-docker build --progress=plain \
-    --build-arg BASE_IMAGE="$BASE_IMAGE" \
-    --build-arg NODES="$NODES" \
-    --build-arg POST_INSTALL=$POST_INSTALL \
-    --build-arg WORKFLOW_TO_COPY=$WORKFLOW_TO_COPY \
-    --build-arg MODELS=$MODELS \
-    --build-arg COMFYUI_VERSION=$COMFYUI_VERSION \
-    --build-arg PYTORCH_WHEEL=$PYTORCH_WHEEL \
-    --build-arg REQ_MODIFY=$REQ_MODIFY \
-    -f /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2/Dockerfile \
-    -t $IMAGE_NAME:$VERSION \
-    /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2
-
-echo -e "\nсобран образ $IMAGE_NAME:$VERSION"
-
+source db.sh
 source dr.sh
-
-#echo "run --name $IMAGE_NAME $IMAGE_NAME:$VERSION"
-#docker rm -f "$IMAGE_NAME"
-#docker run -it --privileged --gpus all -p 8188:8188 -p 1188:1188 \
-#    --env-file .env \
-#    -v /mnt/h/ComfyUI-data/models:/workspace/ComfyUI/models \
-#    --name "$IMAGE_NAME" "$IMAGE_NAME:$VERSION" \
-#    tmux-s-bash.sh
