a1b23b03b8e53ae4277e4bd01729caf5715b6d93
Gayrat Vlasov <ggvlasov@gmail.com>
2025-07-08 06:19:41 +0300

    Consistent_Character_Bondareva

---
+++ comfyui-union2/post-install/Consistent_Character_Bondareva.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e  # Выход при ошибке
+set -x  # Логирование всех команд
+
+#pip install --cache-dir=/root/pip-cache scikit-image blend_modes scikit-learn
+
+rm -rf /root/.cache/pip
+
--- comfyui-union2/post-install/Consistent_Character_Comf_studio.sh
+++ comfyui-union2/post-install/Consistent_Character_Comf_studio.sh
@@ -26,5 +26,25 @@ python3 -m pip install --cache-dir=/root/pip-cache -r requirements.modify.txt
 #63 39.26 Successfully installed colored-2.3.0 cuda-bindings-12.9.0 cuda-python-12.9.0 diffusers-0.24.0 fire-0.7.0 lightning-utilities-0.14.3 onnx-1.15.0 onnxruntime-1.16.3 protobuf-3.20.2 pytorch_lightning-2.5.2 streamdiffusion-0.1.1 torchmetrics-1.7.4 xformers-0.0.31
 #63 39.26 WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager, possibly rendering your system unusable. It is recommended to use a virtual environment instead: https://pip.pypa.io/warnings/venv. Use the --root-user-action option if you know what you are doing and want to suppress this warning.
 
+#ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+#opencv-python 4.12.0.88 requires numpy<2.3.0,>=2; python_version >= "3.9", but you have numpy 1.26.4 which is incompatible.
+#opencv-python-headless 4.12.0.88 requires numpy<2.3.0,>=2; python_version >= "3.9", but you have numpy 1.26.4 which is incompatible.
+#opencv-contrib-python 4.12.0.88 requires numpy<2.3.0,>=2; python_version >= "3.9", but you have numpy 1.26.4 which is incompatible.
+#mediapipe 0.10.14 requires protobuf<5,>=4.25.3, but you have protobuf 3.20.2 which is incompatible.
+#WARNING: Running pip as the 'root' user can result in broken permissions and conflicting behaviour with the system package manager, possibly rendering your system unusable. It is rec
+
+#Error. No artists.csv found. Put your artists.csv in the custom_nodes-ComfyUI_Loader-CSV directory of ComfyUI. Then press "Refresh".                                                                   Your current root directory is: /workspace/ComfyUI
+
+python3 -m pip install --cache-dir=/root/pip-cache \
+  diffusers==0.31.0 \
+  peft \
+  huggingface_hub==0.24.6 \
+  accelerate==1.1.1 \
+  opencv-python \
+  pillow \
+  omegaconf \
+  scikit-image
+
+
 rm -rf /root/.cache/pip
 
