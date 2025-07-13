eaf067afe8152c7bfd3e524bbb8f0cc92026c2ad
Gayrat Vlasov <ggvlasov@gmail.com>
2025-07-13 19:16:30 +0300

    Nunchaku2

--- comfyui-union2/post-install/IPAdapter_plus.sh
+++ comfyui-union2/post-install/IPAdapter_plus.sh
@@ -3,7 +3,6 @@
 set -e  # Выход при ошибке
 set -x  # Логирование всех команд
 
-pip install --cache-dir=/root/pip-cache insightface
-
+python3 -m pip install --cache-dir=/root/pip-cache insightface onnxruntime
 
 rm -rf /root/.cache/pip
