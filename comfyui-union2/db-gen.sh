cbdf5c5a481c07781a39709bdb265a8b2cea88aa
Gayrat Vlasov <ggvlasov@gmail.com>
2025-07-13 15:15:03 +0300

    Nunchaku2

--- comfyui-union2/db-gen.sh
+++ comfyui-union2/db-gen.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 source /mnt/f/_prg/python/Docker-ComfyUI/.venv/bin/activate
+
 cd /mnt/f/_prg/python/Docker-ComfyUI/comfyui-union2
+mkdir -p ./input/"${MODELS}"
+
 python3 ./python-prg-pre-build/generate_dockerfile_v2.py --nodes "${MODELS}.txt"
+
 
 #    --no-cache \
 #    --cache-from $IMAGE_NAME:$VERSION \
