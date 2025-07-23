84f1214cd01481231a64f64da15d5bf5998b9b36
Gayrat Vlasov <ggvlasov@gmail.com>
2025-07-23 08:35:50 +0300

    sageattention_wheel

--- comfyui-union2/post-install/WanTI.sh
+++ comfyui-union2/post-install/WanTI.sh
@@ -6,11 +6,13 @@ set -x  # Логирование всех команд
 # цель triton 3.3 и Sage 2.2)
 # pip list | grep triton
 # =================
-python3 -m pip uninstall -y torch torchaudio triton xformers
 
 python3 -m pip install --cache-dir=/root/pip-cache torch==2.7.0 torchaudio==2.7.0 \
   --extra-index-url https://download.pytorch.org/whl/cu126
 # xformers==0.0.30
 
+python3 -m pip install --cache-dir=/root/pip-cache /workspace/wheels/sageattention-*.whl
+
+rm /workspace/wheels/sageattention-*.whl
 rm -rf /root/.cache/pip
 
