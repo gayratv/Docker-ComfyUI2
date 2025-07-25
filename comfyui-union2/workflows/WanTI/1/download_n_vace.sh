4a8f36ef0c80ab29e281958ed3215d6fa3562e0f
Gayrat Vlasov <ggvlasov@gmail.com>
2025-07-25 03:37:08 +0300

    WAN

---
+++ comfyui-union2/workflows/WanTI/1/download_n_vace.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Model: Native VACE
+# Requires-HF-Token: false
+# Model-URL: https://ali-vilab.github.io/VACE-Page/
+
+echo "Downloading files from HuggingFace repository Comfy-Org/Wan_2.1_ComfyUI_repackaged..."
+
+# Create output directory if it doesn't exist
+mkdir -p "/workspace/ComfyUI/models"
+
+# Download split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors
+aria2c -x 16 -s 16 -d "/workspace/ComfyUI/models/text_encoders" \
+    -o "native_umt5_xxl_fp8_e4m3fn_scaled.safetensors" --auto-file-renaming=false --conditional-get=true --allow-overwrite=true \
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+
+# Download split_files/vae/wan_2.1_vae.safetensors
+aria2c -x 16 -s 16 -d "/workspace/ComfyUI/models/vae" \
+    -o "native_wan_2.1_vae.safetensors" --auto-file-renaming=false --conditional-get=true --allow-overwrite=true \
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+
+
+# Download split_files/diffusion_models/wan2.1_vace_1.3B_fp16.safetensors
+aria2c -x 16 -s 16 -d "/workspace/ComfyUI/models/diffusion_models" \
+    -o "wan2.1_vace_1.3B_fp16.safetensors" --auto-file-renaming=false --conditional-get=true --allow-overwrite=true \
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_1.3B_fp16.safetensors"
+
+# Download split_files/diffusion_models/wan2.1_vace_1.3B_preview_fp16.safetensors
+aria2c -x 16 -s 16 -d "/workspace/ComfyUI/models/diffusion_models" \
+    -o "wan2.1_vace_1.3B_preview_fp16.safetensors" --auto-file-renaming=false --conditional-get=true --allow-overwrite=true \
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_1.3B_preview_fp16.safetensors"
+
+# Download split_files/diffusion_models/wan2.1_vace_14B_fp16.safetensors
+aria2c -x 16 -s 16 -d "/workspace/ComfyUI/models/diffusion_models" \
+    -o "wan2.1_vace_14B_fp16.safetensors" --auto-file-renaming=false --conditional-get=true --allow-overwrite=true \
+    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_vace_14B_fp16.safetensors"
+
+echo "All downloads completed!"
